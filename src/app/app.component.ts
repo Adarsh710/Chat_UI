34e5c6fbc6d69836910892c2965a08740a2f9f8a
Adarsh710 <adarshagrawal777@gmail.com>
2021-01-09 22:20:24 +0530

    added google tag manager

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { NavigationEnd, Router } from "@angular/router";
+import { GoogleTagManagerService } from "angular-google-tag-manager";
 
 @Component({
   selector: 'app-root',
@@ -13,7 +15,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router,
+    private gtmService: GoogleTagManagerService
   ) {
     this.initializeApp();
   }
@@ -22,6 +26,16 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.router.events.forEach(item => {
+        if (item instanceof NavigationEnd) {
+            const gtmTag = {
+                event: 'page',
+                pageName: item.url
+            };
+
+            this.gtmService.pushTag(gtmTag);
+        }
+    });
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { GoogleTagManagerModule } from 'angular-google-tag-manager';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    GoogleTagManagerModule.forRoot({
+      id: 'GTM-KFC4M78',
+    }),
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
